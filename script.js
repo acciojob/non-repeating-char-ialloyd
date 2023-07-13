6e708ae5e3b36f4425eb18c785cf62a474292347
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-13 15:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+
+let map=new Map();
+
+let str=prompt("Enter a string")
+
+function result(str){
+
+	for(let i=0; i<str.length; i++){
+
+		if(map.has(str.charAt(i))){
+
+			map.set(str.charAt(i), map.get(str.charAt(i))+1)
+		}
+		else{
+
+			map.set(str.charAt(i), 1);
+		}
+	}
+
+	let res="";
+	map.forEach((value,key)=>{
+
+		if(value==1){
+
+			res=key;
+			break;
+		}
+	})
+
+	return res;
+	
+}
+
+alert(result(str))
+
+result(str)
